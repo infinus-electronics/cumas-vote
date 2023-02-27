02abcdf9e037e405e92397c7d5673499f46b3d68
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2023-02-27 00:25:52 +0000

    login redirect works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
 <script lang="ts">
-    
-</script> 
+	import { goto } from '$app/navigation';
+	import { currentUser } from '$lib//pocketbase';
+	import { Content } from 'carbon-components-svelte';
+	import { onMount } from 'svelte';
+	import { get } from 'svelte/store';
 
+	onMount(() => {
+		const user = get(currentUser);
+		console.log(user);
+		if (user === null) {
+			goto('/login');
+		}
+	});
+</script>
+
+<Content>
+	{#if $currentUser}
+		<p>Signed in as {$currentUser.username}</p>
+	{/if}
+</Content>
